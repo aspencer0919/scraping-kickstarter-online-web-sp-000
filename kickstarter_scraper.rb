4e8501734da4e079db0e5f629c024db75e062b1c
Adam Spencer <aspencer0919@gmail.com>
2020-05-08 15:08:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 def create_project_hash
 
-
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
@@ -19,25 +18,6 @@ def create_project_hash
     }
     end
   projects
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("ul.project-meta li a span").text
-  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-#  :projects => {
-#  "My Great Project"  => {
-#    :image_link => "Image Link",
-#    :description => "Description",
-#    :location => "Location",
-#    :percent_funded => "Percent Funded"
-#  },
-#  "Another Great Project" => {
-#    :image_link => "Image Link",
-#    :description => "Description",
-#    :location => "Location",
-#    :percent_funded => "Percent Funded"
-#  }
 end
 
 create_project_hash
